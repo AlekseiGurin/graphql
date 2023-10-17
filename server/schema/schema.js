2bd14dfbba6ef131dbce940290c0da9b9a2f3f02
Aleksei Gurin <dmb2009isr@bk.ru>
2023-10-17 21:07:05 +0700

    add queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,14 +1,36 @@
 const  graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
 
+const movies = [
+    {id: '1', name: "Pulp Fiction", genre: "crime"},
+    {id: '2', name: "1986", genre: "crime"},
+    {id: 3, name: "v for vendetta", genre: "triller"},
+    {id: 4, name: "Snatch", genre: "comedy"}
+]
+
+const directors = [
+    {id: "1", name: "Quentin Tarantino", age: 55},
+    {id: "2", name: "Michael Radford", age: 72},
+    {id: "3", name: "James McTeigue", age: 51},
+    {id: "4", name: "Guy Ritchie", age: 50},
+]
 // описываем схему хронящихся в базе
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString },
+        genre: { type: GraphQLInt },
+    })
+});
+
+const DirectorType = new GraphQLObjectType({
+    name: 'Director',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLString },
     })
 });
 
@@ -17,14 +39,24 @@ const MovieType = new GraphQLObjectType({
 const Query = new GraphQLObjectType({
     name: 'Query',
     // подзапрос movie
-    fields: { movie: {
+    fields: {
+        movie: {
         type: MovieType,
-        args: { id: { type: GraphQLString }},
+        args: { id: { type: GraphQLID }},
+        // resolve - метод для манипуляции базой данных
         resolve(parent, args) {
-
-        }
+            return movies.find(movie => args.id === movie.id);
+        },
+      },
+        director: {
+            type: DirectorType,
+            args: { id: { type: GraphQLID }},
+        resolve(parent, args) {
+            return directors.find(director => args.id === director.id);
+        },
+      },
     }
-}})
+});
 
 // экспартируем наш корневой запрос
 module.exports = new GraphQLSchema({
